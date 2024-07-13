df22eb6047fd5bfabe96e1ff5066ae6e034269bb
F4zination <minecraft.neot@gmail.com>
2024-07-13 13:56:52 +0200

    focking shit xml parser

--- src/GPX.jl
+++ src/GPX.jl
@@ -2,6 +2,14 @@ module GPX
 import EzXML
 include("types.jl")
 
+function parse_waypoint(node::EzXML.Node)::WayPoint
+    WayPoint(
+        lat = parse(Float64, node.attributes["lat"]),
+        lon = parse(Float64, node.attributes["lon"]),   
+    )
+end
+
+
 function parse_gpx(path_to_xml::String)
     xml = EzXML.readxml(path_to_xml)
 
@@ -9,6 +17,18 @@ function parse_gpx(path_to_xml::String)
     if root_node.name != "gpx"
         throw(ArgumentError("Root element must be <gpx>"))
     end
-    return root["version"]
+    # waypoints = []
+
+    for waypoint in nodecontent.(findall("/wpt", root_node))
+        println(waypoint)
+    end
+
+    
+        # if hasproperty(element, s::String) s == "wpt"
+        #     waypoint = parse_waypoint(element)
+        #     println(waypoint)
+        # end
+    
+end
 
 end
